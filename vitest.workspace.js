63e707bc66e47e515c5f56184acc1eb0c5daef54
Elioth <dharsuthdra@gmail.com>
2025-05-19 05:44:26 +0700

    Test

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -5,7 +5,10 @@ import { defineWorkspace } from 'vitest/config';
 
 import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineWorkspace([
@@ -15,7 +18,7 @@ export default defineWorkspace([
     plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      storybookTest({ configDir: path.join(dirname, '.storybook') }),
+      // storybookTest({ configDir: path.join(dirname, '.storybook') }),
     ],
     test: {
       name: 'storybook',
